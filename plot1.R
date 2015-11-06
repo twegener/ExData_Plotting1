fa08795bce70076cff61f73fc4463b63d164e456
twegener <twegener@loucon.eu>
2015-11-06 17:17:27 +0100

    Imported from R Studio

---
+++ plot1.R
@@ -0,0 +1,12 @@
+## plot1.R
+# Loading and subsetting overall data set
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+# plot1.R specifics
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
